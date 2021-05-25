0072250d4a8f52fb8e2037c4423dbce00040f0cc
pyroswap <pyroswap@protonmail.com>
2021-05-25 21:18:33 +0200

    Update cake to pyro

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -164,7 +164,7 @@ function SideNav({ history }) {
           <AutoColumn gap="0.5rem" style={{ marginLeft: '.75rem', marginBottom: '4rem' }}>
             <HeaderText>
               <Link href="https://pancakeswap.finance/" target="_blank">
-                PancakeSwap
+                PyroSwap
               </Link>
             </HeaderText>
             <HeaderText>
