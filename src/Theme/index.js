b7fcb2b65ebc1ac79a9c68ae2699fd753b4f03e9
pyroswap <pyroswap@protonmail.com>
2021-05-27 15:20:46 +0200

    Update

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -71,7 +71,7 @@ const theme = (darkMode, color) => ({
   yellow1: '#FFE270',
   yellow2: '#F3841E',
   link: '#12aab5',
-  blue: '2f80ed',
+  blue: '#2f80ed',
 
   background: darkMode ? 'black' : `radial-gradient(50% 50% at 50% 50%, #4FD8DE 30 0%, #fff 0%)`,
 })
